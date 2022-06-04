e58233b48bfdfa53073386f8cc4d65aeb48359ac
Mason Cole <mcole65@gatech.edu>
2022-06-04 08:18:24 -0700

    init argparse args and logging

--- terraform-check-unused-variables.py
+++ terraform-check-unused-variables.py
@@ -3,31 +3,38 @@
 import os
 import re
 import sys
+import argparse
+import logging
 from glob import glob
 
 
-def check_for_unused_vars():
-    variables_file = [tf_files for files in os.walk('.') for tf_files in glob(os.path.join(files[0], '*variables.tf'))]
-    all_tf_files = [tf_files for files in os.walk('.') for tf_files in glob(os.path.join(files[0], '*.tf'))]
-    
+def check_for_unused_vars(args):
+    # variables_file = [tf_files for files in os.walk(args.dir) for tf_files in
+    #                   glob(os.path.join(files[0], '*' + args.var_file))]
+    # all_tf_files = [tf_files for files in os.walk(args.dir) for tf_files in glob(os.path.join(files[0], '*.tf'))]
+    variables_file = glob(os.path.join(args.dir, '*' + args.var_file))
+    all_tf_files = glob(os.path.join(args.dir, '*.tf'))
+
+    logging.debug(f'variable file: {variables_file}')
+    logging.debug(f'tf files: {all_tf_files}')
+
     variables = parse_variables_tf(variables_file[0])
     var_references = find_used_variables(all_tf_files)
     unused_vars = list(variables - var_references)
-    
+
     if len(unused_vars) > 0:
-        print('unused vars detected:')
-        print("%s\n" % unused_vars)
+        logging.info('unused vars detected:')
+        logging.info("%s\n" % unused_vars)
         remove_unused_vars(unused_vars, variables_file[0])
         sys.exit(1)
     else:
-        print('no unused variables found')
+        logging.info('no unused variables found')
         sys.exit(0)
 
 
 def remove_unused_vars(unused_vars, var_file):
     removing_variable = False
     new_file = ''
-    
     with open(var_file, 'r') as file:
         lines = file.readlines()
         for line in lines:
@@ -40,27 +47,29 @@ def remove_unused_vars(unused_vars, var_file):
                 if variable in unused_vars:
                     new_file += ''
                     removing_variable = True
-                    print('removing...' + variable)
+                    logging.info('removing...' + variable)
                 else:
                     new_file += line
             elif not removing_variable:
                 new_file += line
-                
+
     with open(var_file, 'w') as file:
         file.write(new_file)
-        
-        
+
+
 def find_used_variables(tf_files):
     match = []
+    logging.debug(f'searching for unused vars')
     for tf_file in tf_files:
         if 'variables.tf' in tf_file:
+            logging.debug(f'skipping {args.var_file}')
             pass
         with open(tf_file, 'r') as file:
             lines = file.read()
             match += re.findall(r'var\.([\w_]+)', lines)
     return set(match)
-  
-  
+
+
 def parse_variables_tf(var_file):
     temp = set()
     with open(var_file, 'r') as file:
@@ -69,8 +78,40 @@ def parse_variables_tf(var_file):
             if line.startswith('variable'):
                 temp.add(line[line.find("\"") + len("\""):line.rfind("\"")])
     return temp
-  
-  
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--dir',
+                        dest='dir',
+                        default='.',
+                        help='path to search for tf files (default: ".")')
+    parser.add_argument('--var-file',
+                        dest='var_file',
+                        default='variables.tf',
+                        help='path to search for tf files (default: "variables.tf")')
+    parser.add_argument('--verbose', '-v',
+                        dest='debug',
+                        default=False,
+                        action='store_true',
+                        help='flag to show verbose output')
+
+    return parser.parse_args()
+
+
+def init_logger(debug):
+    log_level = logging.INFO
+    if debug:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level, format='%(levelname) -4s: %(message)s')
+    # logging.debug('This message should go to the log file')
+    # logging.info('So should this')
+    # logging.warning('And this, too')
+    # logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+
 if __name__ == '__main__':
-    # args = parse_args()
-    check_for_unused_vars()
+    args = parse_args()
+    init_logger(args.debug)
+    logging.debug(f'args: {vars(args)}')
+    check_for_unused_vars(args)
